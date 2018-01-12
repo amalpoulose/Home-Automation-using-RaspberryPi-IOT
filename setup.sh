ff7fa4530e3c1bd4682fa3f62764142f9a8bd11c
Amal Poulose <amalpoulose1995@hotmail.com>
2018-01-12 10:45:52 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt-get install python
+
+sudo apt-get install python-pip
+
+sudo pip uninstall pubnub
+sudo pip install pubnub==3.9
+
+sudo apt-get install python-picamera
+sudo apt-get install python-picamera-docs
+
+sudo pip install twilio
+sudo apt-get install i2c-tools
